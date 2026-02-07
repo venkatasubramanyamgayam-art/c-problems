2eff8d3d754137cc912cc782e84fc3a06110a14f
venkatasubramanyamgayam-art <venkatasubramanyamgayam@gmail.com>
2026-02-07 22:12:09 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    
+    return 0;
+}
---
+++ p7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+float gpa =89.75;
+printf("%.2f",gpa);
+
+    return 0;
+}
