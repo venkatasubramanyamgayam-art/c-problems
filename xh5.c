8b880b1414eb2a1df614bc038ad267dfddb23190
venkatasubramanyamgayam-art <venkatasubramanyamgayam@gmail.com>
2026-02-24 21:22:46 +0530

    Add files via upload

---
+++ xh5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int marks;
+   printf("enter the marks");
+   scanf("%d",&marks);
+   if (marks>=40)
+   printf("PASS");
+   else 
+   printf("FAIL");
+    //Your Code goes here!
+    
+    return 0;
+}
