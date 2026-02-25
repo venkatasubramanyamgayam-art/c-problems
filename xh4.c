317b4e57bb66f9aa8ee7f2b5efe888b019d282f5
venkatasubramanyamgayam-art <venkatasubramanyamgayam@gmail.com>
2026-02-25 21:14:16 +0530

    Add files via upload

---
+++ xh4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int age;
+  printf("enter the age ");
+  scanf("d",age);
+  if(age>=18)
+  printf("eligible for vote");
+  else 
+  printf(" not eligible for vote");
+    
+    return 0;
+}
