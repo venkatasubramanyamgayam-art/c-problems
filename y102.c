35fb8dfa61eb8a020afbf17e39a92751ecf98746
venkatasubramanyamgayam-art <venkatasubramanyamgayam@gmail.com>
2026-02-25 21:12:32 +0530

    Add files via upload

---
+++ y102.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    printf("enter the number ");
+    scanf("%d",&a);
+    if((a>=10)&&(a<=50))
+    printf("YES");
+    else 
+    printf("NO");
+
+    return 0;
+}
